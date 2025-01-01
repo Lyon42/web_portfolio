dc77c261f191b7dfca429adc30dbd67b11e25141
Leon <leon.m.heinen@gmail.com>
2025-01-01 13:37:29 +0100

    -minor changes

--- install.bat
+++ install.bat
@@ -4,10 +4,10 @@ setlocal
 SET /P AREYOUSURE=Are you sure (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
-call python -m venv \.venv\
-call .venv\Scripts\activate.bat
-call python -m pip install -r \requirements.txt
-call git clone https://github.com/Lyon42/flask_material/ \lib\flask_material\
+call python -m venv ./.venv/
+call ./.venv/Scripts/activate.bat
+call python -m pip install -r ./requirements.txt
+call git clone https://github.com/Lyon42/flask_material/ ./lib/flask_material/
 call python -m flask --app web_portfolio.py init-db
 
 :END
